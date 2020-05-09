0621c3ee6e5b2198c99ac0d4e82ae5dac838b098
Mark A Guza <guzamaguza@gmail.com>
2020-05-09 18:04:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,4 +6,16 @@ class App < Sinatra::Base
       params[:name].reverse
     end
 
+    get '/square/:number' do
+      num = (params[:number].to_i)**2
+      num.to_s
+    end
+
+    get '/reversename/:name' do
+      params[:name].reverse
+    end
+
+    get '/reversename/:name' do
+      params[:name].reverse
+    end
 end
